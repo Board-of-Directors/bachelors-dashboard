f725bff422c84888f29ff21e97acdbdebc4493b9
Fagorym <oleg-veber@mail.ru>
2024-09-28 11:15:00 +0700

    Initial backend commit

---
+++ backend/src/main/kotlin/ru/nsu/fit/bachelors/dashboard/BachelorsDashboardApplication.kt
@@ -0,0 +1,11 @@
+package ru.nsu.fit.bachelors.dashboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BachelorsDashboardApplication
+
+fun main(args: Array<String>) {
+	runApplication<BachelorsDashboardApplication>(*args)
+}
---
+++ backend/src/test/kotlin/ru/nsu/fit/bachelors/dashboard/BachelorsDashboardApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.nsu.fit.bachelors.dashboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BachelorsDashboardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
