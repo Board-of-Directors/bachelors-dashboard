d13dd961106e5041a0304a2548a6c106493ea5da
Fagorym <oleg-veber@mail.ru>
2024-10-28 22:18:14 +0700

    Add spring security + disable cors

---
+++ backend/src/main/kotlin/ru/nsu/fit/bachelors/dashboard/configuration/SecurityConfiguration.kt
@@ -0,0 +1,50 @@
+package ru.nsu.fit.bachelors.dashboard.configuration
+
+import lombok.RequiredArgsConstructor
+import lombok.extern.slf4j.Slf4j
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration {
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .cors {
+                it.configurationSource {
+                    val configuration = CorsConfiguration()
+                    configuration.allowedMethods = ALLOWED_METHODS
+                    configuration.allowCredentials = true
+                    configuration.setAllowedOriginPatterns(ALLOWED_ALL)
+                    configuration.allowedHeaders = ALLOWED_ALL
+                    configuration
+                }
+            }.authorizeHttpRequests { it.anyRequest().permitAll() }
+            .httpBasic(Customizer.withDefaults())
+            .csrf { it.disable() }
+        return http.build()
+    }
+
+    @Bean
+    fun webSecurityCustomizer() = WebSecurity::ignoring
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    companion object {
+        private val ALLOWED_METHODS = listOf("PUT", "POST", "DELETE", "GET", "OPTIONS", "PATCH")
+        private val ALLOWED_ALL = listOf("*")
+    }
+}
