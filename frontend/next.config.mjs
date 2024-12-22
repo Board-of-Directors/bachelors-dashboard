a50ff110ffc24b53ce154c97e69437dce423b206
Tretiakovv <tretiakovvvvv@gmail.com>
2024-12-22 15:35:43 +0700

    feat: add static export config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,16 @@
+const isProd = process.env.NODE_ENV === "production";
+const SITE_PREFIX = "bachelors-dashboard";
+
 const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? `/${SITE_PREFIX}/` : "",
+  basePath: isProd ? `/${SITE_PREFIX}` : "",
+  output: "export",
 };
 
 export default nextConfig;
