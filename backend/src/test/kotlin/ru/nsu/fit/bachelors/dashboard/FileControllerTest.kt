9bb1df89c49853ba6f2b36682f32b835d3ad3489
Fagorym <oleg-veber@mail.ru>
2025-05-04 23:24:42 +0700

    create file tests

--- backend/src/test/kotlin/ru/nsu/fit/bachelors/dashboard/FileControllerTest.kt
+++ backend/src/test/kotlin/ru/nsu/fit/bachelors/dashboard/FileControllerTest.kt
@@ -1,21 +1,21 @@
-package ru.nsu.fit.bachelors.dashboard.config
+package ru.nsu.fit.bachelors.dashboard
 
 import com.github.springtestdbunit.annotation.DatabaseSetup
 import com.github.springtestdbunit.annotation.ExpectedDatabase
-import com.github.springtestdbunit.assertion.DatabaseAssertionMode.NON_STRICT
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import ru.nsu.fit.bachelors.dashboard.AbstractApplicationTest
 import ru.nsu.fit.bachelors.dashboard.dto.common.IdDto
 import ru.nsu.fit.bachelors.dashboard.dto.file.request.FileOrderRequest
+import ru.nsu.fit.bachelors.dashboard.dto.file.response.FileDetailResponse
 import ru.nsu.fit.bachelors.dashboard.dto.file.response.FileDto
 import ru.nsu.fit.bachelors.dashboard.dto.file.response.FilesResponse
 
 @DisplayName("Взаимодействие с файлами")
 @Suppress("NonAsciiCharacters")
+@DatabaseSetup("/database/file/before/file_setup.xml")
 class FileControllerTest : AbstractApplicationTest() {
     @Test
-    @DatabaseSetup("/database/file/before/file_setup.xml")
     fun `Получение всех файлу без группы`() {
         okGet(
             "/api/v1/file",
@@ -34,7 +34,6 @@ class FileControllerTest : AbstractApplicationTest() {
     }
 
     @Test
-    @DatabaseSetup("/database/file/before/file_setup.xml")
     fun `Получение всех файлу по группе`() {
         okGet(
             "/api/v1/file?groupId=100",
@@ -59,8 +58,7 @@ class FileControllerTest : AbstractApplicationTest() {
     }
 
     @Test
-    @DatabaseSetup("/database/file/before/file_setup.xml")
-    @ExpectedDatabase("/database/file/after/added_file.xml", assertionMode = NON_STRICT)
+    @ExpectedDatabase("/database/file/after/added_file.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
     fun `Добавление файла в группу`() {
         okPut(
             "/api/v1/file",
@@ -72,12 +70,39 @@ class FileControllerTest : AbstractApplicationTest() {
     }
 
     @Test
-    @DatabaseSetup("/database/file/before/file_setup.xml")
-    @ExpectedDatabase("/database/file/after/removed_file.xml", assertionMode = NON_STRICT)
+    @ExpectedDatabase("/database/file/after/removed_file.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
     fun `Удаление файла из группы`() {
         okPut(
             "/api/v1/file",
             FileOrderRequest(ids = listOf(IdDto(2000), IdDto(1000))),
+        )
+    }
+
+    @Test
+    fun `Успешное получение детальной информации о документе`() {
+        okGet(
+            "/api/v1/file/detail?fileId=1000",
+            FileDetailResponse(
+                id = 1000,
+                name = "file_1",
+                externalId = "file_ext_1",
+                columns = listOf(),
+                rows = listOf(),
+            ),
+        )
+    }
+
+    @Test
+    fun `Успешное получение детальной информации о таблице`() {
+        okGet(
+            "/api/v1/file/detail?fileId=3000",
+            FileDetailResponse(
+                id = 3000,
+                name = "file_3",
+                externalId = "file_ext_3",
+                columns = listOf(),
+                rows = listOf(),
+            ),
         )
     }
 }
