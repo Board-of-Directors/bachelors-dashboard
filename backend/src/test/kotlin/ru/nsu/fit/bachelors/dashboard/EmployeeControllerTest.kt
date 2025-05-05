8115d787cd3b9ff1ee0aa8866f68bdb5447c7906
Fagorym <oleg-veber@mail.ru>
2025-05-05 22:10:46 +0700

    create test for searching workers

--- backend/src/test/kotlin/ru/nsu/fit/bachelors/dashboard/EmployeeControllerTest.kt
+++ backend/src/test/kotlin/ru/nsu/fit/bachelors/dashboard/EmployeeControllerTest.kt
@@ -10,6 +10,7 @@ import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import ru.nsu.fit.bachelors.dashboard.configuration.UniqueIdentifierGenerator
 import ru.nsu.fit.bachelors.dashboard.dto.employee.request.CreateEmployeeRequest
+import ru.nsu.fit.bachelors.dashboard.dto.employee.response.EmployeeResponseDto
 import ru.nsu.fit.bachelors.dashboard.service.EmailService
 import java.util.UUID
 
@@ -31,6 +32,33 @@ class EmployeeControllerTest(
         verify(mailService).send(
             "Ваш пароль 4f0275f7-d716-4d09-8f28-bc34fa7f0b66 от аккаунта с адрес электронной почты n.konstantinov@g.nsu.ru",
             email = "n.konstantinov@g.nsu.ru",
+        )
+    }
+
+    @Test
+    @ExpectedDatabase(value = "/database/employee/after/deleted_employee.xml", assertionMode = NON_STRICT)
+    fun `Успешное удаление работника`() {
+        okDelete("/api/v1/employee?employeeId=100")
+    }
+
+    @Test
+    fun `Получить всех работников`() {
+        okGet(
+            "/api/v1/employee",
+            listOf(
+                EmployeeResponseDto(id = 100, email = "o.veber@g.nsu.ru"),
+                EmployeeResponseDto(id = 200, email = "a.tretiakov@g.nsu.ru"),
+            ),
+        )
+    }
+
+    @Test
+    fun `Получить работников по фильтрам`() {
+        okGet(
+            "/api/v1/employee/search?email=o.veber@g.nsu.ru",
+            listOf(
+                EmployeeResponseDto(id = 100, email = "o.veber@g.nsu.ru"),
+            ),
         )
     }
 }
