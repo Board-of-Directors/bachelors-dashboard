e22ca38fbde5f70f2bd6c7f5c4c3d5aa83f5a151
Fagorym <oleg-veber@mail.ru>
2025-05-05 18:09:22 +0700

    tests for columns

---
+++ backend/src/test/kotlin/ru/nsu/fit/bachelors/dashboard/ColumnControllerTest.kt
@@ -0,0 +1,26 @@
+package ru.nsu.fit.bachelors.dashboard
+
+import com.github.springtestdbunit.annotation.DatabaseSetup
+import com.github.springtestdbunit.annotation.ExpectedDatabase
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode.NON_STRICT
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import ru.nsu.fit.bachelors.dashboard.dto.column.request.ChangeColumnRequest
+
+@DisplayName("Взаимодействие с колонками таблиц")
+@Suppress("NonAsciiCharacters")
+@DatabaseSetup("/database/table/before/setup.xml")
+class ColumnControllerTest : AbstractApplicationTest() {
+    @Test
+    @ExpectedDatabase("/database/table/after/changed_column.xml", assertionMode = NON_STRICT)
+    fun `Успешное изменение свойств колонки`() {
+        okPut(
+            "/api/v1/column/change",
+            ChangeColumnRequest(
+                columnId = 100,
+                columnHidden = true,
+                columnWidth = 1000,
+            ),
+        )
+    }
+}
