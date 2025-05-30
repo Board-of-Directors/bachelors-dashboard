bb011d61181cc7b929559b0d1b18b3e24d925f1e
Tretiakovv <tretiakovvvvv@gmail.com>
2025-05-30 12:46:38 +0700

    feat: integrate authorization

--- frontend/api/request/auth/types.ts
+++ frontend/api/request/auth/types.ts
@@ -2,3 +2,8 @@ export interface UserCredentials {
   password: string;
   email: string;
 }
+
+export interface AuthCredentials {
+  refreshToken: string;
+  accessToken: string;
+}
--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,1 +1,5 @@
 export * from "./queryKeys";
+
+export const ACCESS_TOKEN_KEY = "fit-access_token";
+
+export const REFRESH_TOKEN_KEY = "fit-refresh-token";
